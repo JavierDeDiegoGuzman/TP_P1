3cbfd14afb6161fe09a3138d1108152541ba031d
Javier <j.dediegoguzman@gmail.com>
2021-09-30 10:05:36 +0200

    update game.java

--- src/es/ucm/tp1/logic/Game.java
+++ src/es/ucm/tp1/logic/Game.java
@@ -1,5 +1,38 @@
 package es.ucm.tp1.logic;
 
-public class Game {
+import es.ucm.tp1.control.Level;
 
+public class Game {
+	
+	public Game(Long seed, Level level) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void reset() {
+		
+	}
+	
+	public String toString () {
+		return "NO";
+	}
+	
+	//Commandos
+	public void goUp(){
+	}
+	
+	public void goDown(){
+	}
+	
+	public void goStraight () {
+	}
+	
+	
+	public void toggleTest() {
+		
+	}
+	
+	
+	
+	
+	
 }
